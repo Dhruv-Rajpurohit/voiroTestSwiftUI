a967de124f2f15e99e4172b06e431a6dc70d276f
Dhruv Rajpurohit <dhruv.n.rajpurohit@gmail.com>
2022-04-30 12:52:51 +0530

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Dhruv Rajpurohit on 30/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
