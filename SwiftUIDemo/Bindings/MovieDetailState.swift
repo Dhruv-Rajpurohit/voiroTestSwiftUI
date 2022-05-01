0afcf4e0a5647e95f071f17df96ba376ba54e27c
Dhruv Rajpurohit <dhruv.n.rajpurohit@gmail.com>
2022-05-01 12:11:00 +0530

    MovieDetailView and State files added

---
+++ SwiftUIDemo/Bindings/MovieDetailState.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDetailState.swift
+//  SwiftUIDemo
+//
+//  Created by Dhruv Rajpurohit on 01/05/22.
+//
+
+import Foundation
---
+++ SwiftUIDemo/Views/MovieDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailView.swift
+//  SwiftUIDemo
+//
+//  Created by Dhruv Rajpurohit on 01/05/22.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailView()
+    }
+}
