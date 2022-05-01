aab331a344fa5acfa31d416bfcf432d501244595
Dhruv Rajpurohit <dhruv.n.rajpurohit@gmail.com>
2022-05-01 11:12:05 +0530

    BackdropCard SwiftUI View Added

---
+++ SwiftUIDemo/Views/MovieBackdropCard.swift
@@ -0,0 +1,20 @@
+//
+//  MovieBackdropCard.swift
+//  SwiftUIDemo
+//
+//  Created by Dhruv Rajpurohit on 01/05/22.
+//
+
+import SwiftUI
+
+struct MovieBackdropCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieBackdropCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieBackdropCard()
+    }
+}
