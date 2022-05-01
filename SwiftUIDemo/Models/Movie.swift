7b043dbd6db4a440a71d4062356e535c12df1160
Dhruv Rajpurohit <dhruv.n.rajpurohit@gmail.com>
2022-05-01 12:58:23 +0530

    Added computed properties in Movie model

--- SwiftUIDemo/Models/Movie.swift
+++ SwiftUIDemo/Models/Movie.swift
@@ -11,7 +11,7 @@ struct MovieResponse: Decodable {
     let results : [Movie]
 }
 
-struct Movie: Decodable, Identifiable{
+struct Movie: Decodable, Identifiable {
     let id: Int
     let title: String
     let backdropPath: String?
@@ -20,6 +20,21 @@ struct Movie: Decodable, Identifiable{
     let voteAverage: Double
     let voteCount: Int
     let runtime: Int?
+    let releaseDate: String?
+    let genres: [MovieGenre]?
+    
+    static private let yearFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy"
+            return formatter
+        }()
+    
+    static private let durationFormatter: DateComponentsFormatter = {
+            let formatter = DateComponentsFormatter()
+            formatter.unitsStyle = .full
+            formatter.allowedUnits = [.hour, .minute]
+            return formatter
+        }()
     
     var backdropURL: URL {
         return URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath ?? "")")!
@@ -28,6 +43,41 @@ struct Movie: Decodable, Identifiable{
     var posterURL: URL {
         return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath ?? "")")!
     }
+    
+    var genreText: String {
+        genres?.first?.name ?? "n/a"
+    }
+    
+    var ratingText: String {
+            let rating = Int(voteAverage)
+            let ratingText = (0..<rating).reduce("") { (acc, _) -> String in
+                return acc + "★"
+            }
+            return ratingText
+        }
+        
+        var scoreText: String {
+            guard ratingText.count > 0 else {
+                return "n/a"
+            }
+            return "\(ratingText.count) / 10"
+        }
+        
+        var yearText: String {
+            guard let releaseDate = self.releaseDate, let date = Utils.dateFormatter.date(from: releaseDate) else {
+                return "n/a"
+            }
+            return Movie.yearFormatter.string(from: date)
+        }
+        
+        var durationText: String {
+            guard let runtime = self.runtime, runtime > 0 else {
+                return "n/a"
+            }
+            return Movie.durationFormatter.string(from: TimeInterval(runtime) * 60) ?? "n/a"
+        }
 }
 
-
+struct MovieGenre: Decodable {
+    let name: String
+}
