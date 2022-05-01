2171b5f6aa35a4789ea639be8571d5b2467e8941
Dhruv Rajpurohit <dhruv.n.rajpurohit@gmail.com>
2022-05-01 12:07:40 +0530

    Added MovieList View carousel of all 4 genres to show in vStack and have horizontal scroll

--- SwiftUIDemo/Views/MovieListView.swift
+++ SwiftUIDemo/Views/MovieListView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct MovieListView: View {
+    
+    @ObservedObject private var nowPlayingState = MovieListState()
+    @ObservedObject private var upcomingState = MovieListState()
+    @ObservedObject private var topRatedState = MovieListState()
+    @ObservedObject private var popularState = MovieListState()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Group {
+                    if nowPlayingState.movies != nil {
+                        MoviePosterCarouselView(title: "Now Playing", movies: nowPlayingState.movies!)
+                        
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
+                
+                Group {
+                    if upcomingState.movies != nil {
+                        MovieBackdropCarouselView(title: "Upcoming", movies: upcomingState.movies!)
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                
+                
+                Group {
+                    if topRatedState.movies != nil {
+                        MovieBackdropCarouselView(title: "Top Rated", movies: topRatedState.movies!)
+                        
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                
+                Group {
+                    if popularState.movies != nil {
+                        MovieBackdropCarouselView(title: "Popular", movies: popularState.movies!)
+                        
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 16, trailing: 0))
+                
+            }
+            .navigationBarTitle("The MovieDb")
+        }
+        .onAppear {
+            self.nowPlayingState.loadMovies(with: .nowPlaying)
+            self.upcomingState.loadMovies(with: .upcoming)
+            self.topRatedState.loadMovies(with: .topRated)
+            self.popularState.loadMovies(with: .popular)
+        }
+        
     }
 }
 
